ff853468597a1a16b39831396fc1a3dbb96771e0
kevans38 <31311258+kevans38@users.noreply.github.com>
2017-12-03 14:55:49 -0500

    Create cbcmac_validate.java

---
+++ cbcmac_validate.java
@@ -0,0 +1,84 @@
+//validates a tag for a given file and tag
+import java.util.Arrays;
+
+
+public class cbcmac_validate {
+	static int BIT_BLOCK_SIZE = 128;
+	//need to divide by 8 because it will be in BYTES
+	// so 128 bits / 8 = 16 bytes
+	static int BLOCK_SIZE = BIT_BLOCK_SIZE/8;
+	
+	public static void main(String[] args) throws Exception
+	{
+		boolean validate = false;
+		//use test_printing(byte[]) to print out a byte array
+		byte[] key_data;
+		byte[] input_data;
+		byte[] iv_data;
+		byte[] tag = null;
+		byte[] intag = null;
+		byte[] message_length = new byte[BLOCK_SIZE];
+		
+		byte[][] input_blocks;
+
+		byte[] cipher_text;
+		
+		byte[] temp_data = null;
+		
+		int input_size;
+		
+		//open files from cmd line args
+		key_data = ctfuncs.key_file(args);
+		input_data = ctfuncs.input_file_msg(args);
+		input_size = input_data.length;
+		
+		//get tag from file to compare later
+		intag = ctfuncs.input_file_intag(args);
+		
+		//iv_data = ctfuncs.iv_file(args);
+
+		Arrays.fill(message_length, (byte)0);
+		message_length[0] = (byte)input_data.length;
+		iv_data = message_length;// first iv is message length
+		
+		//create blocks and pad - using 128 bit blocks
+		input_blocks = ctfuncs.make_blocks(input_data, input_size);
+		
+		input_blocks = ctfuncs.padding(input_blocks, input_size);
+		
+		//+1 for the IV at the front
+		cipher_text = new byte[(input_blocks.length+1)*BLOCK_SIZE];
+		
+		cipher_text = ctfuncs.append_bytes(cipher_text, iv_data, 0);
+		
+		//XOR Message: 1st with IV, then remaining with ciphertext
+		for ( int i = 0; i < input_blocks.length; i++ )
+		{
+			if ( i == 0 )
+			{
+				temp_data = ctfuncs.xor_bytes(input_blocks[i], iv_data);
+			}
+			else
+			{
+				temp_data = ctfuncs.xor_bytes(input_blocks[i], temp_data);
+			}
+			
+			temp_data = ctfuncs.encrypt_data(temp_data, key_data);
+			
+			//don't need this because just getting the last block
+			//cipher_text = ctfuncs.append_bytes(cipher_text, temp_data, i+1);
+
+		}
+		tag = temp_data;
+		
+		//ctfuncs.test_printing(tag);
+		//ctfuncs.test_printing(intag);
+		
+		if(Arrays.equals(tag, intag)){
+			validate = true;
+		}
+		
+		//Prints True or False for validation
+		System.out.println(validate);
+	}
+}
