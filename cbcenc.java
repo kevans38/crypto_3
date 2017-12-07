71f92f0b3b2f07e35796f8873d94415f0583c501
cdavid11 <cdavid11@vols.utk.edu>
2017-12-07 08:59:46 -0500

    Trying new cbcenc

--- cbcenc.java
+++ cbcenc.java
@@ -1,7 +1,3 @@
-import java.math.BigInteger;
-import java.util.Arrays;
-
-
 public class cbcenc
 {
 	static int BIT_BLOCK_SIZE = 128;
@@ -9,76 +5,67 @@ public class cbcenc
 	// so 128 bits / 8 = 16 bytes
 	static int BLOCK_SIZE = BIT_BLOCK_SIZE/8;
 	
-	byte[] cipher_text;
-	byte[] key_data;
-	byte[] input_data;
-	byte[] iv_data;
-	
-	public cbcenc(byte[] key_data, byte[] input_data, byte[] iv_data){
-		this.key_data = key_data;
-		this.input_data = input_data;
-		this.iv_data = iv_data;
-		encrypt();
-	}
-	
-	public void encrypt(){
+	byte [] cipher_text;
+	byte [] input_data;
+
+	public cbcenc (byte[] key_data, String filename){
+
+		byte[] iv_data;
 		byte[][] input_blocks;
-		int input_size = input_data.length;
 		byte[] temp_data = null;
+
+		int input_size;
+		try{
+			input_data = ctfuncs.read_file(filename);
+			input_size = input_data.length;
+
+			iv_data = ctfuncs.IVgen();
+
+			input_blocks = ctfuncs.make_blocks(input_data, input_size);
+			input_blocks = ctfuncs.padding(input_blocks, input_size);
+
+			cipher_text = new byte [(input_blocks.length+1)*BLOCK_SIZE];
+
+			cipher_text = ctfuncs.append_bytes(cipher_text, iv_data, 0);
 		
-		//create blocks and pad - using 128 bit blocks
-		input_blocks = ctfuncs.make_blocks(input_data, input_size);
-		
-		input_blocks = ctfuncs.padding(input_blocks, input_size);
-		
-		//+1 for the IV at the front
-		cipher_text = new byte[(input_blocks.length+1)*BLOCK_SIZE];
-		
-		//---------------must have iv_data set, 1st is 0's , then is output of each 
-		cipher_text = ctfuncs.append_bytes(cipher_text, this.iv_data, 0);
-		
-		//XOR Message: 1st with IV, then remaining with ciphertext
-		for ( int i = 0; i < input_blocks.length; i++ )
-		{
-			if ( i == 0 )
-			{
-				temp_data = ctfuncs.xor_bytes(input_blocks[i], this.iv_data);
-			}
-			else
-			{
-				temp_data = ctfuncs.xor_bytes(input_blocks[i], temp_data);
-			}
+			for ( int i = 0; i < input_blocks.length; i++){
+
+				if (i == 0){
+
+					temp_data = ctfuncs.xor_bytes(input_blocks[i], iv_data);
+				}else{
+				
+					temp_data = ctfuncs.xor_bytes(input_blocks[i], temp_data);
+
+				}
 			
-			//YELLING AT THIS STATEMENT----------------------------
-			temp_data = ctfuncs.encrypt_data(temp_data, this.key_data);
-			
-			cipher_text = ctfuncs.append_bytes(cipher_text, temp_data, i+1);
+				temp_data = ctfuncs.encrypt_data(temp_data, key_data);
+				cipher_text = ctfuncs.append_bytes(cipher_text, temp_data, i+1);
+
+			}
+
+		} catch (Exception e){
+
+			System.out.println("Error");
+			System.exit(1);
+		
 
 		}
 
 	}
-	
-	
+
 	public static void main(String[] args) throws Exception
 	{
 		//use test_printing(byte[]) to print out a byte array
-
-		byte[] key;
-		byte[] input;
-		byte[] iv = new byte[100];	//should be n bytes long 0n, length of message
-		cbcenc C;
+		byte[] key_data;
+		byte[] input_data;
+		byte[] iv_data;
 		
 		//open files from cmd line args
-		key = ctfuncs.key_file(args);
-		input = ctfuncs.input_file(args);
-			
-		//fill array to 0n to start
-		Arrays.fill(iv, (byte)0);
-		
-		//call the cbc constructor with key, input, and starting iv value
-		C = new cbcenc(key, input, iv);
+		key_data = ctfuncs.key_file(args);
+		input_data = ctfuncs.input_file(args);
 		
 		//create encrypted file
-		//ctfuncs.output_file(args, cipher_text );
+		//ctfuncs.output_file(args, cipher_text);
 	}
 }
